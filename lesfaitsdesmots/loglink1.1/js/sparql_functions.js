d3ba4770b6c6834642d5321bfdb21eb47e44d55b
fluidlog <lesontologiesgraphiques@gmail.com>
2014-11-03 19:18:06 +0100

    Séparation des fonctions js qui font appelles au SPARQL dans un fichier

---
+++ lesfaitsdesmots/loglink1.1/js/sparql_functions.js
@@ -0,0 +1,403 @@
+//=======================================
+//
+// Fonctions AJAX SPARQL de l'interface loglink1.1.js
+//
+//=======================================
+
+function sparql_exist_domain(domain_name)
+{
+	var test;
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+		{
+		    type: 'GET',
+		    url: '../../sparql/main.php',
+		    dataType: 'json',
+		    success: function(t_data) 
+					    { 
+							// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> retour,
+		    				affiche_debug(t_data[0], t_data[1]);
+							if (t_data[2] != null)
+							{
+								//True or false
+								test = t_data[2];
+							}
+							return false;
+					    },
+		    error: function(t_data) 
+					    { 
+							message ("Erreur Ajax : Message="+t_data+" (Fonction sparql_exist_domain()) !","alert");
+					    },
+		    data: { fn : "existDomain", domain_name : domain_name },
+		    async: false
+		}
+	);
+	return test;
+}
+
+
+function sparql_add_domain_to_triplestore(domain_name)
+{
+	var return_domain;
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+		{
+		    type: 'GET',
+		    url: '../../sparql/main.php',
+		    dataType: 'json',
+		    success: function(t_data) 
+					    { 
+							// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> retour,
+		    				affiche_debug(t_data[0], t_data[1]);
+							if (t_data[2] != null)
+							{
+								return_domain = t_data[2];
+							}
+							return false;
+					    },
+		    error: function(t_data) 
+					    { 
+							message ("Erreur Ajax : Message="+t_data+" (Fonction sparql_add_domain_to_triplestore()) !","alert");
+					    },
+		    data: { fn : "addDomainToTriplestore", domain_name : domain_name },
+		    async: false
+		}
+	);
+	return return_domain;
+}
+
+function sparql_increment_node_id(node_iri_id)
+{
+	var node_iri_id;
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+		{
+		    type: 'GET',
+		    url: '../../sparql/main.php',
+		    dataType: 'json',
+		    success: function(t_data) 
+					    { 
+							// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> retour,
+							affiche_debug(t_data[0], t_data[1]);
+							if (t_data[2] != null)
+							{
+								node_iri_id = t_data[2];
+							}
+							return false;
+					    },
+		    error: function(t_data) 
+					    { 
+							message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_increment_node_id()) !","alert");
+		    				return false;
+					    },
+		    data: { fn : "incrementNodeId", node_iri_id : node_iri_id },
+		    async: false
+		}
+	);
+	return node_iri_id;
+}
+
+function sparql_get_new_node_iri()
+{
+	var node_iri;
+	
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+			{
+			    type: 'GET',
+			    url: '../../sparql/main.php',
+			    dataType: 'json',
+			    success: function(t_data) 
+						    { 
+					 			if (t_data[2] != null)
+			 					{
+									// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> iri,
+				    				affiche_debug(t_data[0], t_data[1]);
+				    				node_iri=t_data[2];
+			 					}
+								else
+								{
+									message ("Erreur t_data Null","alert");
+								}
+						    },
+			    error: function(t_data) 
+						    { 
+			    				message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_get_new_node_iri()) !","alert");
+			    				return false;
+						    },
+			    data: { fn : "getNewNodeIri" },
+			    async: false
+			}
+		);
+
+	return node_iri;
+}
+
+function sparql_init_kernel_graph(first_node_iri, second_node_iri)
+{
+	var nodes = [];
+	var edges = [];
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+		{
+		    type: 'GET',
+		    url: '../../sparql/main.php',
+		    dataType: 'json',
+		    success: function(t_data) 
+					    { 
+							// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> retour,
+							affiche_debug(t_data[0], t_data[1]);
+				 			if (t_data[2] != null)
+		 					{
+				 				message ("Kernel initialized","succed");
+							}
+			 		 		return false;
+					    },
+		    error: function(t_data) 
+					    { 
+							message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_init_kernel_graph()) !","alert");
+		    				return false;
+					    },
+		    data: { fn : "initKernelGraph", first_node_iri : first_node_iri, second_node_iri : second_node_iri },
+		    async: false
+		}
+	);
+	return { nodes : nodes, edges : edges };
+}
+
+//Récupère les termes à l'utilisateur connecté
+function sparql_get_dataset()
+{
+	var nodes = [];
+	var edges = [];
+	
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+			{
+			    type: 'GET',
+			    url: '../../sparql/main.php',
+			    dataType: 'json',
+			    success: function(t_data) 
+				    { 
+						// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> user,
+	    				affiche_debug(t_data[0], t_data[1]);
+			 			if (t_data[2] != null)
+	 					{		
+			 					//Récupération des noeuds dans le tableau nodes
+			 				
+			 					//Attention, pour éviter que la fonction "d3.force" ne plante, 
+			 					//il faut autant de noeuds que d'indice existants (iri_id)
+			 					//Donc on va ajouter des noeuds "fictifs" pour combler les trous ;-)
+				 		 		if (t_data[2].nodes)
+				 		 		{
+					 		 		$.each(t_data[2].nodes, function(i, entry)
+											{
+					 		 					nodes.push({
+					 								iri_id : parseInt(entry.node_iri_id, 10), 
+					 								label : entry.node_label, 
+					 								type : entry.node_type, 
+												});
+											}
+									);
+					 		 		//Ne pas oublier d'ordonner les noeuds, car l'indice correspond à celui des liens !
+			 						nodes = nodes.sort(function (a,b) {return a.iri_id - b.iri_id});
+
+				 		 			for (inc=0; inc<nodes.length; inc++)
+				 		 			{
+				 		 				if (nodes[inc].iri_id != inc)
+				 		 					{
+				 		 						//Ajout d'un noeud fictif à la position "inc", 0 supression.
+					 		 					nodes.splice(inc, 0, {
+					 								iri_id : -1, 
+					 								label : "deleted", 
+					 								type : "deleted", 
+												});
+				 		 					}
+				 		 			}
+				 		 		}
+				 		 					 						
+			 					//récupération des liens dans le tableau edges
+		 						//Important de convertir les index en type "integer" pour la fonciton D3 force
+				 		 		if (t_data[2].edges)
+				 		 		{
+			 						$.each(t_data[2].edges, function(i, entry)
+											{	
+					 		 					edges.push({
+					 								source : parseInt(entry.source, 10), 
+					 								target : parseInt(entry.target, 10), 
+												});
+											}
+					 		 		);
+				 		 		}
+
+				 		 		message ("Dataset refreshed","succed");
+	 					}
+						else
+						{
+		    				affiche_debug(t_data[0], t_data[1]);
+							message ("No term in triplestore for the user connected","warning");
+						}
+				    },
+			    error: function(t_data) 
+						    {
+			    				message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_get_dataset()) !","alert");
+			    				return false;
+						    },
+			    data: { fn : "getDataset" },
+			    async: false
+			}
+		);
+
+	return { nodes : nodes, edges : edges };
+}
+
+// Cette fonction ajoute un nouveau noeud et lien entre lui et le noeud à partir duquel il a été créé.
+function sparql_add_node(source_node_id, target_node_id)
+{
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+		{
+		    type: 'GET',
+		    url: '../../sparql/main.php',
+		    dataType: 'json',
+		    success: function(t_data) 
+					    { 
+							// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> retour,
+							affiche_debug(t_data[0], t_data[1]);
+				 			if (t_data[2] != null)
+		 					{
+				 				message ("Node ["+t_data[2].target+"] added, link ["+t_data[2].source+" > "+t_data[2].target+"] added","succed");
+							}
+			 		 		return false;
+					    },
+		    error: function(t_data) 
+					    { 
+							message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_add_node()) !","alert");
+		    				return false;
+					    },
+		    data: { fn : "addNode", source_node_id : source_node_id, target_node_id : target_node_id },
+		    async: false
+		}
+	);
+}
+
+//Cette fonction ajoute un lien lorsque l'utilisateur veut faire un lien entre deux noeuds existants 
+function sparql_add_link(source_node_id, target_node_id)
+{
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+		{
+		    type: 'GET',
+		    url: '../../sparql/main.php',
+		    dataType: 'json',
+		    success: function(t_data) 
+					    { 
+							// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> retour,
+							affiche_debug(t_data[0], t_data[1]);
+				 			if (t_data[2] != null)
+		 					{
+				 				message ("Link ["+t_data[2].source+" > "+t_data[2].target+"] added","succed");
+							}
+			 		 		return false;
+					    },
+		    error: function(t_data) 
+					    { 
+							message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_add_link()) !","alert");
+		    				return false;
+					    },
+		    data: { fn : "addLink", source_node_id : source_node_id, target_node_id : target_node_id },
+		    async: false
+		}
+	);
+}
+
+function sparql_delete_link (source_iri_id, target_iri_id)
+{
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+		{
+		    type: 'GET',
+		    url: '../../sparql/main.php',
+		    dataType: 'json',
+		    success: function(t_data) 
+					    { 
+							// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> retour,
+							affiche_debug(t_data[0], t_data[1]);
+							if (t_data[2] != null)
+							{
+								result = t_data[2];
+							}
+							return false;
+					    },
+		    error: function(t_data) 
+					    { 
+							message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_delete_link()) !","alert");
+		    				return false;
+					    },
+		    data: { fn : "deleteLink", source_iri_id : source_iri_id, target_iri_id : target_iri_id},
+		    async: false
+		}
+	);
+}
+
+function sparql_delete_node (node_iri_id)
+{
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+		{
+		    type: 'GET',
+		    url: '../../sparql/main.php',
+		    dataType: 'json',
+		    success: function(t_data) 
+					    { 
+							// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> retour,
+							affiche_debug(t_data[0], t_data[1]);
+							if (t_data[2] != null)
+							{
+								result = t_data[2];
+							}
+							return false;
+					    },
+		    error: function(t_data) 
+					    { 
+							message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_delete_node()) !","alert");
+		    				return false;
+					    },
+		    data: { fn : "deleteNode", node_iri_id : node_iri_id},
+		    async: false
+		}
+	);
+}
+
+function sparql_delete_all_into_triplestore()
+{
+	//Appelle main.php de manière synchrone. C'est à dire, attend la réponse avant de continuer
+	$.ajax(
+			{
+			    type: 'GET',
+			    url: '../../sparql/main.php',
+			    dataType: 'json',
+			    success: function(t_data) 
+						    { 
+					 			if (t_data[2] != null)
+			 					{
+									// t_data[0] -> nom fonction php, t_data[1] -> sparql, t_data[2] -> iri,
+				    				affiche_debug(t_data[0], t_data[1]);
+					 				message ("delete_all_into_triplestore OK","succed");
+			 					}
+								else
+								{
+									message ("Erreur t_data Null","alert");
+								}
+						    },
+			    error: function(t_data) 
+						    { 
+			    				message ("Erreur Ajax : Message="+t_data.responseText+" (Fonction sparql_delete_all_into_triplestore()) !","alert");
+			    				return false;
+						    },
+			    data: { fn : "deleteAll" },
+			    async: false
+			}
+		);
+}
+
